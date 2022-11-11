627dcdf6ac25ad9045b254bf5f8ecd5d49e0dcab
Barry Archer <barcher975@gmail.com>
2022-11-11 11:03:30 -0600

    initial imoprts for render and htmlStrucure

--- public/main.js
+++ public/main.js
@@ -1,47 +1,7 @@
-// import '../styles/main.scss'; // You have to import your styles for them to work. Comment in this line
-
-const students = [];
-const voldysArmy = [];
-const houses = [
-  {
-    house: 'gryffindor',
-    crest:
-      'https://static.wikia.nocookie.net/pottermore/images/1/16/Gryffindor_crest.png'
-  },
-  {
-    house: 'slytherin',
-    crest:
-      'https://static.wikia.nocookie.net/pottermore/images/4/45/Slytherin_Crest.png'
-  },
-  {
-    house: 'hufflepuff',
-    crest:
-      'https://static.wikia.nocookie.net/pottermore/images/5/5e/Hufflepuff_crest.png'
-  },
-  {
-    house: 'ravenclaw',
-    crest:
-      'https://static.wikia.nocookie.net/pottermore/images/4/4f/Ravenclaw_crest.png'
-  }
-];
-
-const renderToDOM = (divId, content) => {
-  const selectedDiv = document.querySelector(divId);
-  selectedDiv.innerHTML = content;
-};
-
-// ********** HTML Components  ********** //
-// the basic HMTL structure of app
-const htmlStructure = () => {
-  const domString = `
-  <div id="header-container" class="header mb-3"></div>
-  <div id="form-container" class="container mb-3 text-center"></div>
-  <div id="filter-container" class="container mb-3"></div>
-  <div id="student-container" class="container d-flex"></div>
-  `;
-
-  renderToDOM('#app', domString);
-};
+import renderToDOM from '../components/renderToDom';
+import '../styles/main.scss'; // You have to import your styles for them to work. Comment in this line
+import { houses, voldysArmy, students } from '../components/data/dataForApp';
+import htmlStructure from '../components/htmlStructure';
 
 const header = () => {
   const domString = `<div class="container">
